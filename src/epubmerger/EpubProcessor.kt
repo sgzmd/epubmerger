aeacb657f6e9ed89bd6291bf2ad0957cf87e0d6b
Roman Kirillov <sigizmund@gmail.com>
2018-12-28 20:42:14 +0000

    Reprocessing resources is now working, to a degree

--- src/epubmerger/EpubProcessor.kt
+++ src/epubmerger/EpubProcessor.kt
@@ -3,6 +3,7 @@ package epubmerger
 import nl.siegmann.epublib.domain.Book
 import nl.siegmann.epublib.domain.Resource
 import nl.siegmann.epublib.epub.EpubReader
+import java.nio.charset.Charset
 import java.nio.file.Path
 
 /**
@@ -11,34 +12,98 @@ import java.nio.file.Path
 class EpubProcessor(files: List<Path>) {
 
   private var files: List<Path> = files
-  private lateinit var book: Book
-  private var epubIndex = 0
+  internal var book = Book()
+  internal lateinit var hrefIdMap: Map<String, Pair<String, String>>
 
   fun mergeFiles() {
     val epubs = readFiles()
-    book = Book()
 
+    val hrefIdMap = calculateResourceNames(epubs)
     for (epub in epubs) {
-      if (book.coverImage == null) {
-
+      if (epub.coverPage != null && epub.coverImage != null) {
+        buildCoverPage(epub)
+        break
       }
     }
   }
 
-  internal fun buildCoverPage(epub: Book) {
-    if (epub.coverImage != null && epub.coverPage != null) {
-      book.coverImage = Resource(epub.coverImage.data, epub.coverImage.mediaType)
+  internal fun calculateResourceNames(epubs: List<Book>): HashMap<String, Pair<String, String>> {
+    // Old href to new href/id
+    val map = HashMap<String, Pair<String, String>>()
+    epubs.forEachIndexed { index, epub ->
+      epub.resources.all.forEachIndexed { resIdx, resource ->
+        val ext = getFileExtension(resource.href)
+        val id = "id_${index}_${resIdx}"
+        val href = "href_${index}_${resIdx}.$ext"
 
-      val reprocessed = reprocessResource(epub.coverPage, epub.coverImage.href, book.coverImage.href)
-      if (reprocessed != null) {
-        book.coverPage = Resource(reprocessed, epub.coverPage.mediaType)
+        map.put(resource.href, Pair(href, id))
       }
     }
+
+    return map
+  }
+
+  internal fun reprocessResources(epubs: List<Book>) {
+    epubs.forEach { epub ->
+      epub.resources.all.forEach { res ->
+        val hrefIdPair = hrefIdMap[res.href]
+        val data: ByteArray = res.data
+        book.addResource(Resource(
+            hrefIdPair?.second,
+            reprocessResourceData(res.data, res.mediaType.toString()),
+            hrefIdPair?.first,
+            res.mediaType))
+      }
+    }
+  }
+
+  internal fun reprocessResourceData(data: ByteArray, type: String): ByteArray {
+    if (SUPPORTED_MEDIA_TYPES.contains(type)) {
+      // This is a text type we understand, to a degree, OK to reprocess
+      val str = String(data, Charset.forName("UTF-8"))
+
+      // Very naive and non-efficient implementation for now
+      var result = str
+      for (oldHref in hrefIdMap.keys) {
+        val quotedHref = "\"$oldHref\""
+        val newQuotedHref = "\"${hrefIdMap[oldHref]?.first}\""
+        while (result.indexOf(quotedHref) > 0) {
+          result = result.replace(quotedHref, newQuotedHref)
+        }
+      }
+
+      return result.toByteArray()
+    } else {
+      return data
+    }
+  }
+
+
+  internal fun buildCoverPage(epub: Book) {
+    assert(hrefIdMap != null, { "hrefIdMap must be initialised" })
+    val newImageHrefId: Pair<String, String>? = hrefIdMap[epub.coverImage.href]
+    val newCoverPageHrefId = hrefIdMap[epub.coverPage.href]
+
+    book.coverImage = book.resources.getByHref(newImageHrefId?.first)
+    book.coverPage = book.resources.getByHref(newCoverPageHrefId?.first)
   }
 
   internal fun readFiles(): List<Book> {
     return this.files.map {
       EpubReader().readEpub(it.toFile().inputStream())
     }
+  }
+
+  companion object {
+    internal fun getFileExtension(fileName: String): String {
+      val dotIdx = fileName.indexOf('.')
+      if (dotIdx >= 0) {
+        return fileName.substring(dotIdx + 1)
+      } else {
+        return ""
+      }
+    }
+
+    val SUPPORTED_MEDIA_TYPES = setOf("text/html", "application/xhtml+xml", "text/plain", "text/xml", "application/xml")
   }
 }
---
+++ tests/EpubProcessorTests.kt
@@ -0,0 +1,74 @@
+import epubmerger.EpubProcessor
+import nl.siegmann.epublib.domain.Book
+import nl.siegmann.epublib.epub.EpubReader
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.nio.file.Paths
+import java.util.regex.Matcher
+import java.util.regex.Pattern
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+import kotlin.test.assertTrue
+
+class EpubProcessorTest {
+
+    lateinit var book: Book
+    lateinit var sut: EpubProcessor
+
+    @BeforeEach
+    fun setUp() {
+        val fis = Paths.get("./testdata/chekov.epub").toFile().inputStream()
+        book = EpubReader().readEpub(fis)
+        sut = EpubProcessor(emptyList())
+
+    }
+
+    @Test
+    fun testReprocessResources() {
+        val map = sut.calculateResourceNames(listOf(book))
+        sut.hrefIdMap = map
+        sut.reprocessResources(listOf(book))
+        map.forEach { oldHref, hrefIdPair ->
+            assertTrue(book.resources.containsByHref(oldHref))
+            assertTrue(sut.book.resources.containsByHref(hrefIdPair.first))
+            assertTrue(sut.book.resources.containsId(hrefIdPair.second))
+        }
+    }
+
+    @Test
+    fun testReprocessResourceData() {
+        sut.hrefIdMap = sut.calculateResourceNames(listOf(book))
+
+        // very small chapter, but contains reference to style.css
+        val ch2 = book.resources.getByHref("ch2.xhtml")
+        val result = sut.reprocessResourceData(ch2.data, "application/xhtml+xml")
+
+//        The text should be in this form:
+//        <....>
+//        <link rel="stylesheet" href="href_0_7.css" type="text/css"/>
+//        <....>
+//
+//        Although the name of the resource is not guaranteed
+
+        val pattern = Pattern.compile(".+href=\"href_[0-9]+_[0-9]+\\.css\".+", Pattern.MULTILINE)
+        assertTrue(pattern.matcher(String(result).replace('\n', ' ')).matches())
+
+        assertNotEquals(result, ch2.data)
+    }
+
+    @Test
+    fun testGetFileExtension_HasExtension() {
+        assertEquals("name", EpubProcessor.getFileExtension("file.name"))
+    }
+
+    @Test
+    fun testGetFileExtension_NoExtension() {
+        assertEquals("", EpubProcessor.getFileExtension("filename"))
+    }
+
+    @Test
+    fun testGetFileExtension_HiddenFile() {
+        assertEquals("filename", EpubProcessor.getFileExtension(".filename"))
+    }
+
+}
