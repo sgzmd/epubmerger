19f1763ae4d4ef1b0872b40ed362dd8965558964
Roman Kirillov <sigizmund@gmail.com>
2019-01-08 17:25:51 +0000

    More code in BookMerger

--- epubmergelib/src/epubmerger/BookMerger.kt
+++ epubmergelib/src/epubmerger/BookMerger.kt
@@ -4,9 +4,7 @@ import nl.siegmann.epublib.domain.Book
 import nl.siegmann.epublib.domain.Resource
 import nl.siegmann.epublib.domain.TOCReference
 
-class ResourceManager(var epubs: List<Book>) {
-  data class TocResource(val resourceHref: String, val resourceId: String, val fragment: String?, val title: String)
-
+class BookMerger(var epubs: List<Book>) {
   val result = Book()
 
   internal fun calculateResourceNames() {
@@ -15,14 +13,15 @@ class ResourceManager(var epubs: List<Book>) {
 
   internal fun calculateResourceNames(book: Book, index: Int) {
     book.tableOfContents.tocReferences.forEach {
-      processTOCReference(index, it, book)
+      processTOCReference(index, it, book, null)
     }
   }
 
-  private fun processTOCReference(bookIndex: Int, sourceTocReference: TOCReference, book: Book) {
+  private fun processTOCReference(bookIndex: Int, sourceTocReference: TOCReference, book: Book, parent: TOCReference?) {
     val href = "${bookIndex}_${sourceTocReference.resource.href}"
     if (!result.resources.containsByHref(href)) {
       val id = "${bookIndex}_${sourceTocReference.resourceId}"
+
       // TODO: reprocess resource data
       val resource = Resource(id, sourceTocReference.resource.data, href, sourceTocReference.resource.mediaType)
       book.resources.add(resource)
@@ -34,6 +33,12 @@ class ResourceManager(var epubs: List<Book>) {
     else
       TOCReference(sourceTocReference.title, resource, sourceTocReference.fragmentId)
 
-    result.tableOfContents.addTOCReference(tocReference)
+    var resultTOCReference = if (parent == null)
+      result.tableOfContents.addTOCReference(tocReference)
+    else parent.addChildSection(tocReference)
+
+    sourceTocReference.children.forEach {
+      processTOCReference(bookIndex, it, book, resultTOCReference)
+    }
   }
 }
