a9177c5b7d09068c4c97deef1e0bd39f547a5928
Roman Kirillov <sigizmund@gmail.com>
2019-01-08 17:45:26 +0000

    Even More code in BookMerger

--- epubmergelib/src/epubmerger/BookMerger.kt
+++ epubmergelib/src/epubmerger/BookMerger.kt
@@ -7,13 +7,37 @@ import nl.siegmann.epublib.domain.TOCReference
 class BookMerger(var epubs: List<Book>) {
   val result = Book()
 
-  internal fun calculateResourceNames() {
+  fun mergeBooks() {
+    epubs.forEachIndexed { index, epub ->
+      if (epub.tableOfContents != null && epub.tableOfContents.size() > 0) {
+        processTOC(epub, index)
+      } else {
+        // If there's no TOC in the book we have to just take
+        // items from spine and process one after another
+        epub.spine.spineReferences.forEach { spineReference ->
+
+        }
+      }
+    }
+
+    // and now go over all the resources which we may not have included and make
+    // sure they are, in fact, included. Or should we do it on resource reprocessing
+    // stage instead?
+  }
+
+  internal fun processTOC() {
 
   }
 
-  internal fun calculateResourceNames(book: Book, index: Int) {
-    book.tableOfContents.tocReferences.forEach {
-      processTOCReference(index, it, book, null)
+  internal fun processTOC(book: Book, index: Int) {
+    book.tableOfContents.tocReferences.forEach { tocReference ->
+      // TODO: use addSection and pass the result as parent
+
+      if (book.coverPage != null) {
+
+      }
+
+      processTOCReference(index, tocReference, book, null)
     }
   }
 
@@ -37,8 +61,8 @@ class BookMerger(var epubs: List<Book>) {
       result.tableOfContents.addTOCReference(tocReference)
     else parent.addChildSection(tocReference)
 
-    sourceTocReference.children.forEach {
-      processTOCReference(bookIndex, it, book, resultTOCReference)
+    sourceTocReference.children.forEach { childTocEntry ->
+      processTOCReference(bookIndex, childTocEntry, book, resultTOCReference)
     }
   }
 }
