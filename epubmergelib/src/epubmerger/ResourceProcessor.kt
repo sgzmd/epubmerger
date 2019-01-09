28c7d3af8c332961a03c084aad9116aa2a4c014e
Roman Kirillov <sigizmund@gmail.com>
2019-01-09 15:54:28 +0000

    Resource processing is working now, although not adding resources anymore and no recursive processing. We also have tests.

--- epubmergelib/src/epubmerger/ResourceProcessor.kt
+++ epubmergelib/src/epubmerger/ResourceProcessor.kt
@@ -2,17 +2,46 @@ package epubmerger
 
 import nl.siegmann.epublib.domain.Book
 import nl.siegmann.epublib.domain.MediaType
-import nl.siegmann.epublib.domain.Resource
 import org.jsoup.Jsoup
+import java.net.URI
 import java.nio.file.Paths
 
 object ResourceProcessor {
+
+  class FullHref(fullHref: String) {
+    val uri: URI = URI(fullHref)
+    val href: String
+      get() = uri.path
+
+    val fragment: String?
+      get() = uri.fragment
+
+    fun withUpdatedHref(newHref: String): String {
+      return if (fragment == null) {
+        newHref
+      } else {
+        "$newHref#$fragment"
+      }
+    }
+
+  }
+
   private val SUPPORTED_SRC_ATTRS = listOf("src", "href")
 
-  // TODO this will fail miserably if we have a circular refence, i.e. chapter 1 refers
-  // TODO to footnotes page, and footnotes page refers back - rare but not inconceivable.
-  // TODO We need to move away from recursion here, however nasty that sounds.
-  fun reprocessXhtmlFile(data: ByteArray, book: Book, index: Int, result: Book): ByteArray {
+  /**
+   * Updates XHTML file so that:
+   * <ul>
+   *   <li>It is now a well-formed XHTML</li>
+   *   <li>All link/a/img tags in the document are up-to-date with the new resource names</li>
+   * </ul>
+   *
+   * @param data an XHTML file in UTF-8, potentially non-well-formed. See also: Resource#getData()
+   * @param book a Book object from which resource above was taken
+   * @param index An index of the book in the final result
+   *
+   * @return a ByteArray with reprocessed valid XHTML
+   */
+  fun reprocessXhtmlFile(data: ByteArray, book: Book, index: Int): ByteArray {
     val soup = Jsoup.parse(String(data))
     val elements = soup.select("*")
 
@@ -25,32 +54,19 @@ object ResourceProcessor {
           continue
         }
 
-        val href: String = it.attr(attr)
-        if (!book.resources.containsByHref(href)) {
+        val href = FullHref(it.attr(attr))
+        if (!book.resources.containsByHref(href.href)) {
           continue
         }
 
         // Constructing new EpubResource descriptor
-        val res = book.resources.getByHref(href)
+        val res = book.resources.getByHref(href.href)
         val newEpubResource = createResourceName(res.href, res.id, index)
-        if (!result.resources.containsByHref(newEpubResource.newHref)) {
-          // If newHref is not in the result's resources yet, let's reprocess it and add it -
-          // it may or may not be referenced by TOC, better reprocess it here.
-          var data: ByteArray? = null
-          if (res.mediaType.isTextBasedFormat) {
-            data = reprocessXhtmlFile(res.data, book, index, result)
-          } else {
-            data = res.data
-          }
-
-          // Adding resource, now we can change attr to it
-          result.resources.add(Resource(newEpubResource.newId, data, newEpubResource.newHref, res.mediaType))
-        }
+        it.attr(attr, href.withUpdatedHref(newEpubResource.newHref!!))
       }
     }
 
-    // TODO fix me
-    return "".toByteArray()
+    return soup.toString().toByteArray()
   }
 
   fun createResourceName(href: String, id: String, bookIndex: Int): EpubResource {
@@ -60,10 +76,10 @@ object ResourceProcessor {
     else
       "${bookIndex}_${path.fileName}"
 
-    val newid = "${bookIndex}_${id}"
-
-    return EpubResource(href, bookIndex, newhref, newid)
+    return EpubResource(href, bookIndex, newhref, "${bookIndex}_${id}")
   }
+
+
 }
 
 private val MediaType.isTextBasedFormat: Boolean
--- epubmergelib/tests/ResourceProcessorTest.kt
+++ epubmergelib/tests/ResourceProcessorTest.kt
@@ -1,7 +1,10 @@
 package epubmerger
 
 import com.google.common.truth.Truth.assertThat
+import nl.siegmann.epublib.epub.EpubReader
+import org.jsoup.Jsoup
 import org.junit.Test
+import java.nio.file.Paths
 
 class ResourceProcessorTest {
   @Test
@@ -15,4 +18,34 @@ class ResourceProcessorTest {
         "very/long/path/image.png", "1", 1)
     assertThat(veryLongPathImagePng.newHref).isEqualTo("very/long/path/1_image.png")
   }
+
+  @Test
+  fun testFullHref() {
+    val noFragmentHref = "images/image.png"
+    val noFragment = ResourceProcessor.FullHref(noFragmentHref)
+    assertThat(noFragment.href).isEqualTo("images/image.png")
+    assertThat(noFragment.fragment).isNull()
+
+    val withFragmentHref = "chapters/chapter.xhtml#footnote"
+    val withFragment = ResourceProcessor.FullHref(withFragmentHref)
+    assertThat(withFragment.href).isEqualTo("chapters/chapter.xhtml")
+    assertThat(withFragment.fragment).isEqualTo("footnote")
+  }
+
+  @Test
+  fun testReprocess() {
+    val fis = Paths.get("./testdata/chekov.epub").toFile().inputStream()
+    val book = EpubReader().readEpub(fis)
+    val data = book.resources.getByHref("ch1.xhtml").data
+    val newData = ResourceProcessor.reprocessXhtmlFile(data, book, 1)
+
+
+    val soup = Jsoup.parse(String(newData))
+    val a = soup.select("a")[0]
+    assertThat(a.attr("href")).isEqualTo("1_ch2.xhtml#id1")
+
+    val link = soup.select("link")[0]
+    assertThat(link.attr("href")).isEqualTo("1_style.css")
+  }
 }
+
