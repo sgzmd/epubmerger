b03971c0f1cdd90b66f863f5096df5fdbedaf958
Roman Kirillov <sigizmund@gmail.com>
2019-01-24 15:46:06 +0000

    Preparing for metadata editor

---
+++ app/app/src/main/java/com/sigizmund/epubmergeapp/MetadataActivity.kt
@@ -0,0 +1,13 @@
+package com.sigizmund.epubmergeapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MetadataActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_metadata)
+  }
+}
