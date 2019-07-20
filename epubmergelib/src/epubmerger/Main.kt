b0d63f85644f42eb026d4eb7ae0f1bb0f2d5f419
Roman Kirillov <sigizmund@gmail.com>
2019-07-20 13:39:15 +0100

    Better CLI version.
    Since there are no plans to build desktop UI version, building better CLI is a good idea. Using an argparser library
    instead of raw positional args sounds like a good idea at this point.

--- epubmergelib/src/epubmerger/Main.kt
+++ epubmergelib/src/epubmerger/Main.kt
@@ -1,26 +1,44 @@
 package epubmerger
 
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.InvalidArgumentException
+import com.xenomachina.argparser.default
 import nl.siegmann.epublib.epub.EpubReader
 import java.nio.file.FileSystems
 import java.nio.file.Files
 import java.nio.file.Paths
 
+class MergeArgs(parser: ArgParser) {
+  val epubs by parser.positionalList("SOURCE", "Epub files to merge", 1..Int.MAX_VALUE)
+      .addValidator {
+        if (value.size <= 1) {
+          throw InvalidArgumentException("One expects more than one epub to merge")
+        }
+      }
+  val output by parser.storing("-o", "--output", help = "Output epub path")
+  val author by parser.storing("-A", "--author", help = "Merged book author name").default("")
+  val title by parser.storing("-T", "--title", help = "Title of the merged book").default("")
+}
+
 fun main(args: Array<String>) {
-  if (args.size < 3) {
-    println("Usage: java -jar /path/to/jar output.epub /path/to/source/directory/file1.epub /path/to/source/directory/file2.epub ... ")
-    return
+  ArgParser(args).parseInto(::MergeArgs).run {
+    println("Merging files: ${epubs.joinToString(",")}")
+
+    val ep = BookMerger(epubs
+        .map { Paths.get(it) }
+        .map { EpubReader().readEpub(it.toFile().inputStream()) })
+
+    if (!title.isNullOrBlank()) {
+      ep.mergedBookTitle = title
+    }
+
+    if (!author.isNullOrBlank()) {
+      ep.mergedBookAuthor = author
+    }
+
+    ep.mergeBooks()
+
+    ep.writeBook(Paths.get(output))
+
   }
-
-  val outputFileName = args[0]
-
-  val fileNames = args.toList().drop(1)
-
-  println(fileNames)
-
-  val ep = BookMerger(fileNames
-    .map { Paths.get(it) }
-    .map { EpubReader().readEpub(it.toFile().inputStream()) })
-  ep.mergeBooks()
-
-  ep.writeBook(Paths.get(outputFileName))
 }
